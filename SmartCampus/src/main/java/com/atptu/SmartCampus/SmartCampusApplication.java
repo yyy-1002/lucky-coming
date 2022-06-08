059a2fb0aa77c3479afbc0bd1a544650eaa255fa
yyy-1002 <100980294+yyy-1002@users.noreply.github.com>
2022-06-08 22:29:46 +0800

    Add files via upload

---
+++ SmartCampus/src/main/java/com/atptu/SmartCampus/SmartCampusApplication.java
@@ -0,0 +1,13 @@
+package com.atptu.SmartCampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartCampusApplication.class, args);
+	}
+
+}
